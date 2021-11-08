26a167433fe197bb69ff352a762181cf4dd097ee
MrEmmirul21 <emmirulmirul@gmail.com>
2021-11-08 10:39:11 +0000

    lab3

---
+++ signal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+
+int main()
+{
+ void sigint_handler(int sig);
+ char s[200];
+
+ if( signal(SIGINT,sigint_handler) == SIG_ERR )
+ {
+  perror("signal");
+  exit(1);
+ }
+
+ printf("Enter a string: ");
+
+ if( fgets(s,200,stdin) == NULL )
+  perror("Gets");
+ else
+  printf("You entered: %s\n",s);
+
+ return 0;
+}
+
+void sigint_handler(int sig)
+{
+ printf(" Please don't disturb me!\n");
+}
